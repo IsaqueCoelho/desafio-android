160d303922abe3eeaa6b7fea833e1b51840bb63e
Isaque <isaquecoelho.trabalho@hotmail.com>
2022-02-04 01:18:12 -0300

    task: get data from database and sync from api;

--- app/src/main/java/com/picpay/desafio/android/contacts/repository/ContactRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/contacts/repository/ContactRepository.kt
@@ -1,14 +1,53 @@
 package com.picpay.desafio.android.contacts.repository
 
+import android.util.Log
+import com.picpay.desafio.android.contacts.User
+import com.picpay.desafio.android.data.ContactDao
+import com.picpay.desafio.android.data.UserEntity
 import kotlinx.coroutines.flow.flow
-import retrofit2.Retrofit
 
 class ContactRepository(
-    private val service: PicPayService
+    private val service: PicPayService,
+    private val contactDao: ContactDao
 ) {
 
     fun getContactList() = flow {
-        val contactList = service.getUsers()
-        emit(contactList)
+        val localContactList = contactDao.selectContacts()
+        when {
+            localContactList.isNullOrEmpty() -> {
+                Log.e("ContactRepository", "sem cache")
+                val newContactList = syncData()
+                emit(newContactList)
+            }
+            else -> {
+                Log.e("ContactRepository", "com cache")
+                val contactList = localContactList.map {
+                    User(
+                        id = it.id,
+                        name = it.name,
+                        username = it.username,
+                        img = it.img,
+                    )
+                }
+
+                emit(contactList)
+                syncData()
+            }
+        }
+    }
+
+    private suspend fun syncData(): List<User> {
+        val newContactList = service.getUsers()
+        val entityContactList = newContactList.map {
+            UserEntity(
+                id = it.id,
+                name = it.name,
+                username = it.username,
+                img = it.img,
+            )
+        }
+
+        contactDao.insertContacts(entityContactList)
+        return newContactList
     }
 }
